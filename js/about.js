503faee27551e777390fd4de4bff531b082f40a7
Pedro Almeida <palmeida2016@fau.edu>
2024-06-24 16:37:35 -0400

    small work to player variables

--- js/about.js
+++ js/about.js
@@ -4,11 +4,18 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+// Constants
+const gravity = 1
+
 class Player {
     constructor () {
         this.position = {
             x: 100,
             y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
         }
         this.width = 100
         this.height = 100
@@ -16,6 +23,14 @@ class Player {
 
     draw() {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+
+    }
+
+    move(){
+
     }
 }
 
